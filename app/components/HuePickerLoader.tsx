c49103fb4161da7c52a83eca5b57298cc0e25b69
alan-augusto <augustoalan56@gmail.com>
2026-03-26 13:47:07 -0300

    feat: Restrict HuePicker component rendering to development environments.

--- app/components/HuePickerLoader.tsx
+++ app/components/HuePickerLoader.tsx
@@ -5,6 +5,6 @@ import dynamic from "next/dynamic";
 const HuePicker = dynamic(() => import("./HuePicker"), { ssr: false });
 
 export default function HuePickerLoader() {
-  // if (process.env.NODE_ENV !== "development") return null;
+  if (process.env.NODE_ENV !== "development") return null;
   return <HuePicker />;
 }
